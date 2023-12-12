23e17a7b6fa179615677ae112fc1ef29525c3aa6
Maciej Siwak <maciej.siwak@deptagency.com>
2023-12-12 16:01:42 +0000

    Updated

--- tests/a11y.spec.ts
+++ tests/a11y.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@playwright/test';
 import { completeA11yScanOnPage, completeA11yScanOnPageReportInConsole, completeA11yScanOnSitemap } from './a11y';
 
-test.only('Check for a11y issues on a single page @a11y', async ({ page }) => {
+test('Check for a11y issues on a single page @a11y', async ({ page }) => {
   await completeA11yScanOnPage("https://www.deptagency.com/", page);
 });
 
